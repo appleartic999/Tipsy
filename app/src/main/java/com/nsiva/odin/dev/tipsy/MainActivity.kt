922c23bae3b8306e36e71d86af372d9f30f9d6f0
Apple Artic <techapple652@gmail.com>
2022-12-31 12:17:19 +0530

    created layout of the app

--- app/src/main/java/com/nsiva/odin/dev/tipsy/MainActivity.kt
+++ app/src/main/java/com/nsiva/odin/dev/tipsy/MainActivity.kt
@@ -2,10 +2,27 @@ package com.nsiva.odin.dev.tipsy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var etBaseAmount: EditText
+    private lateinit var etTipPercentage: EditText
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
+    private lateinit var seekBar: SeekBar
+    private lateinit var tvShareAmount: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        etTipPercentage = findViewById(R.id.etTipPercentage)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        seekBar = findViewById(R.id.seekBar)
+        tvShareAmount = findViewById(R.id.tvShareAmount)
     }
 }
